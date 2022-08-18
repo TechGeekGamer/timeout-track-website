b8fd2827b0aef92a2be12e7e4bce6e8c283b809d
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-08-17 22:53:30 -0700

    switch upcoming features to a component

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,7 +1,6 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 import NotFoundPage from "../pages/404/404";
 import Home from "../pages/home/Home";
-import UpcomingFeatures from "../pages/upcomingFeatures/UpcomingFeatures";
 import Images from "../pages/images/Images";
 import BuildPatchModal from "../BuildPatchModal";
 
@@ -13,7 +12,6 @@ function Main() {
         <div className="content">
           <Routes>
             <Route path="/" element={Home()} />
-            <Route path="/upcoming" element={UpcomingFeatures()} />
             <Route path="/images" element={Images()} />
             <Route path="*" element={NotFoundPage()} />
           </Routes>
